6bd5944be3f639134de31f52e65c4ab2beed3b15
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2023-12-14 09:35:24 -0300

    add submission for day 4 part 2

--- 2023/04/main.go
+++ 2023/04/main.go
@@ -16,8 +16,8 @@ func main() {
 	total := calculateResultForPart1(data)
 	fmt.Println("Result Part 1: " + fmt.Sprint(total))
 
-	// total = calculateResultForPart2(data)
-	// fmt.Println("Result Part 2: " + fmt.Sprint(total))
+	totalPart2 := calculateResultForPart2(data)
+	fmt.Println("Result Part 2: " + fmt.Sprint(totalPart2))
 
 }
 
@@ -27,19 +27,7 @@ func calculateResultForPart1(data []string) float64 {
 	for _, line := range data {
 		slog.Info("Processing line", slog.String("line", line))
 
-		cardTotal := 0
-
-		splitStr := strings.Split(strings.Split(line, ":")[1], "|")
-		myCard := strings.Fields(splitStr[0])
-		winningCard := splitStr[1]
-
-		for _, number := range myCard {
-			hasNumberOnWinningCard := regexp.MustCompile(`(^|\s+)` + number + `(\s+|$)`).Match([]byte(winningCard))
-			if hasNumberOnWinningCard {
-				slog.Debug("Winning number detected", slog.String("number", number))
-				cardTotal++
-			}
-		}
+		cardTotal := calculateCardPoints(line)
 
 		if cardTotal > 0 {
 			total += math.Pow(2, float64(cardTotal-1))
@@ -49,6 +37,45 @@ func calculateResultForPart1(data []string) float64 {
 	return total
 }
 
-func calculateResultForPart2(data []string) float64 {
-	panic("unimplemented")
+func calculateResultForPart2(data []string) int {
+	scratchcardCopies := make([]int, len(data))
+	total := 0
+	const ORIGINAL_SCRATCHPAD_COPY = 1
+
+	for i, line := range data {
+		slog.Info("Processing line", slog.String("line", line))
+
+		cardTotal := calculateCardPoints(line)
+
+		for j := 1; j <= cardTotal && i+j < len(data); j++ {
+			if scratchcardCopies[i] > 0 {
+				scratchcardCopies[i+j] += scratchcardCopies[i]
+			}
+
+			scratchcardCopies[i+j] += ORIGINAL_SCRATCHPAD_COPY
+		}
+	}
+
+	for _, copies := range scratchcardCopies {
+		total += copies
+	}
+
+	return total + len(data)
+}
+
+func calculateCardPoints(line string) int {
+	cardTotal := 0
+
+	splitStr := strings.Split(strings.Split(line, ":")[1], "|")
+	myCard := strings.Fields(splitStr[0])
+	winningCard := splitStr[1]
+
+	for _, number := range myCard {
+		hasNumberOnWinningCard := regexp.MustCompile(`(^|\s+)` + number + `(\s+|$)`).Match([]byte(winningCard))
+		if hasNumberOnWinningCard {
+			slog.Debug("Winning number detected", slog.String("number", number))
+			cardTotal++
+		}
+	}
+	return cardTotal
 }
