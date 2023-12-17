fd3d328a9e7fa79f8d04b4ec3ad82b5c716637e3
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2023-12-16 23:20:26 -0300

    add submission for day 7 part 2

--- 2023/07/main.go
+++ 2023/07/main.go
@@ -12,32 +12,35 @@ func main() {
 	common.InitSlogLogger(slog.LevelWarn)
 	data := common.ReadTestFileContentInLines("input.txt")
 
-	hands := readHands(data)
+	hands := readHands(data, 1)
 
-	total := calculateResultForPart1(hands)
+	total := calculateResult(hands)
 	fmt.Println("Result Part 1: " + fmt.Sprint(total))
 
-	total = calculateResultForPart2(hands)
+	hands = readHands(data, 2)
+
+	total = calculateResult(hands)
 	fmt.Println("Result Part 2: " + fmt.Sprint(total))
 }
 
-func readHands(data []string) (hands []Hand) {
+func readHands(data []string, version int) (hands []Hand) {
 	for _, line := range data {
 		parsedLine := strings.Fields(line)
-		hands = append(hands, *NewHand(parsedLine[0], parsedLine[1]))
+		if version == 1 {
+			hands = append(hands, *NewHand(parsedLine[0], parsedLine[1]))
+		} else {
+			hands = append(hands, *NewHandPart2(parsedLine[0], parsedLine[1]))
+		}
+
 	}
 	return hands
 }
 
-func calculateResultForPart1(hands []Hand) (total int) {
+func calculateResult(hands []Hand) (total int) {
 	sort.Slice(hands, func(i, j int) bool { return hands[i].compare(hands[j]) })
 
 	for i, h := range hands {
 		total += (i + 1) * h.bid
 	}
 	return
-}
-
-func calculateResultForPart2(hands []Hand) int {
-	return 0
 }
--- 2023/07/types.go
+++ 2023/07/types.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"sort"
 	"strconv"
 
@@ -18,8 +19,8 @@ const (
 )
 
 var CHAR_TO_VALUE_MAP = map[byte]int{
-	'A': 0, 'K': 1, 'Q': 2, 'J': 3, 'T': 4, '9': 5, '8': 6,
-	'7': 7, '6': 8, '5': 9, '4': 10, '3': 11, '2': 12,
+	'A': 0, 'K': 1, 'Q': 2, 'T': 4, '9': 5, '8': 6,
+	'7': 7, '6': 8, '5': 9, '4': 10, '3': 11, '2': 12, 'J': 13,
 }
 
 type Hand struct {
@@ -67,6 +68,80 @@ func (h Hand) getType() int {
 				return OnePair
 			}
 			if tryFullHouse {
+				return ThreeOfAKind
+			}
+		}
+	}
+	return None
+}
+
+func NewHandPart2(content string, bid string) *Hand {
+	h := new(Hand)
+	h.content = content
+	h.bid, _ = strconv.Atoi(bid)
+	h.typeOf = h.getTypePart2()
+	return h
+}
+
+func (h Hand) getTypePart2() int {
+	charactersMap := make(map[rune]int)
+	for _, c := range h.content {
+		charactersMap[c]++
+	}
+
+	repetitions := maps.Values(charactersMap)
+	tryFullHouse := false
+	tryTwoPair := false
+	sort.Slice(repetitions, func(i, j int) bool { return repetitions[i] > repetitions[j] })
+	for _, v := range repetitions {
+		switch v {
+		case 5:
+			return FiveOfAKind
+		case 4:
+			if charactersMap['J'] == 1 || charactersMap['J'] == 4 {
+				return FiveOfAKind
+			}
+			return FourOfAKind
+		case 3:
+			if charactersMap['J'] == 1 {
+				return FourOfAKind
+			}
+			if charactersMap['J'] == 2 {
+				return FiveOfAKind
+			}
+			tryFullHouse = true
+		case 2:
+			switch {
+			case tryTwoPair && charactersMap['J'] == 2:
+				if slices.Contains(repetitions[1:], 2) {
+					return FourOfAKind
+				}
+			case tryFullHouse:
+				if charactersMap['J'] == 3 {
+					return FiveOfAKind
+				}
+				return FullHouse
+			case tryTwoPair && charactersMap['J'] == 1:
+				return FullHouse
+			case tryTwoPair:
+				return TwoPair
+			}
+			tryTwoPair = true
+		case 1:
+			switch {
+			case tryTwoPair && charactersMap['J'] == 1:
+				return ThreeOfAKind
+			case tryTwoPair && charactersMap['J'] == 2:
+				return ThreeOfAKind
+			case charactersMap['J'] == 3:
+				return FourOfAKind
+			case charactersMap['J'] == 2:
+				return FourOfAKind
+			case charactersMap['J'] == 1:
+				return OnePair
+			case tryTwoPair:
+				return OnePair
+			case tryFullHouse:
 				return ThreeOfAKind
 			}
 		}
